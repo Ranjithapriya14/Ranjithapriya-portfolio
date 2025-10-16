65dd2ebe0897fbf25195d4e5ff38b44ed7045a6e
RANJITHA PRIYA <167100474+Ranjithapriya14@users.noreply.github.com>
2025-10-16 23:37:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-ScrollReveal().reveal('.hero-left', { origin: 'left', distance: '50px', duration: 1000, delay: 300 });
-ScrollReveal().reveal('.hero-right', { origin: 'right', distance: '50px', duration: 1000, delay: 500 });
-ScrollReveal().reveal('.navbar', { origin: 'top', distance: '20px', duration: 800, delay: 200 });
+ScrollReveal().reveal('.hero-left', { 
+	origin: 'left',
+	distance: '50px',
+	duration: 1000,
+	delay: 300 
+});
+ScrollReveal().reveal('.navbar', {
+	 origin: 'top',
+	 distance: '20px',
+	 duration: 800,
+	 delay: 200
+ });
 
 ScrollReveal().reveal('.about-img', {
   origin: 'left',
